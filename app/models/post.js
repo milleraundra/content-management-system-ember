37a4caa864ab7614b353c5884cdd8ddf0e48505a
Michelle Brecunier and Mary Warrington <marywarrington@gmail.com>
2016-04-12 10:08:52 -0700

    add properties to post and site models

--- app/models/post.js
+++ app/models/post.js
@@ -1,5 +1,8 @@
 import Model from 'ember-data/model';
 
 export default Model.extend({
-  
+  title: DS.attr(),
+  content: DS.attr(),
+  author: DS.attr(),
+  site: DS.belongsTo('site', { async: true })
 });
