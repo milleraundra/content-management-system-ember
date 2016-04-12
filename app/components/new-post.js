10c01be7dc65491c392853cf361b97e7c5404916
Michelle Brecunier and Mary Warrington <michelle.brecunier@gmail.com>
2016-04-12 11:59:19 -0700

    Add new post save functionality

--- app/components/new-post.js
+++ app/components/new-post.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    save() {
+      var params = {
+        title: this.get('title'),
+        author: this.get('author'),
+        content: this.get('content'),
+        site: this.get('site')
+      };
+      this.sendAction('save', params);
+    }
+  }
 });
--- app/routes/new-post.js
+++ app/routes/new-post.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('post');
+  },
+  actions: {
+    save(params) {
+      var newPost = this.store.createRecord('post', params);
+      newPost.save();
+      this.transitionTo('admin');
+    }
+  }
 });
