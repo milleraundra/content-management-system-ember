46be44fedea604b196274ae1a7fed2e65648f048
Michelle Brecunier and Mary Warrington <marywarrington@gmail.com>
2016-04-12 16:37:37 -0700

    create delete-site component

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+  return Ember.RSVP.hash({
+    sites: this.store.findAll('site'),
+    posts: this.store.findAll('post')
+    });
+  }
 });
