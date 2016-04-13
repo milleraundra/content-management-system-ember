0083cac3f9644568ecfe6486265282eb15e6c23d
Aundra Miller and Chris Young <miller.aundra@gmail.com>
2016-04-13 11:51:21 -0700

    create model in edit-site

--- app/routes/edit-site.js
+++ app/routes/edit-site.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    // return this.RSVP.hash({
+    //   site: this.store.findRecord('site', params.site.id),
+    //   posts: this.store.query('post', {
+    //     orderBy: 'site',
+    //     equalTo: params.site.id
+    //   })
+    // });
+  }
 });
