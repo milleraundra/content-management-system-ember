645c1f2d62c81fa74c985fec50cded2104749a3b
Aundra Miller <miller.aundra@gmail.com>
2016-04-14 09:34:34 -0700

    update admin routes to require authentication

--- app/router.js
+++ app/router.js
@@ -6,11 +6,11 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('new-site');
-  this.route('new-post', {path: ':site_id/new-post'});
+  this.authenticatedRoute('new-site');
+  this.authenticatedRoute('new-post', {path: ':site_id/new-post'});
   this.authenticatedRoute('admin');
-  this.route('edit-post', {path: '/edit-post/:post_id'});
-  this.route('edit-site', {path: '/edit-site/:site_id'});
+  this.authenticatedRoute('edit-post', {path: '/edit-post/:post_id'});
+  this.authenticatedRoute('edit-site', {path: '/edit-site/:site_id'});
   this.route('site', {path: '/:site_id'});
   this.route('view-post', {path: '/view-post/:post_id'});
 });
