f3576c7946f8f1a00f3064ff3839b2b243a67969
Michelle Brecunier and Mary Warrington <michelle.brecunier@gmail.com>
2016-04-14 08:36:09 -0700

    Add delete site component on edit site route

--- app/components/delete-site.js
+++ app/components/delete-site.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    delete(site) {
+      if (confirm("Are you sure you want to delete this site?")) {
+        this.sendAction('delete', site);
+      }
+    }
+  }
 });
--- app/routes/edit-site.js
+++ app/routes/edit-site.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('site', params.site_id)
+  },
+  actions: {
+    delete(site) {
+      var post_deletions = site.get('posts').map(function(post) {
+        return post.destroyRecord();
+      });
+      Ember.RSVP.all(post_deletions).then(function() {
+        return site.destroyRecord();
+      });
+      this.transitionTo('admin');
+    }
   }
 });
